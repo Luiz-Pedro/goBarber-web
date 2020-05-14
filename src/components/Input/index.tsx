8adbf2b0367052480ac81cf42e70461a779db9d1
luizmaciel <luizmaciel@denox.com.br>
2020-05-14 09:55:42 -0300

    authorization and toasts

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,6 +8,7 @@ import React, {
 import { IconBaseProps } from 'react-icons';
 import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
+
 import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -59,7 +60,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
                 {...rest}
             />
             {error && (
-                <Error>
+                <Error title={error}>
                     <FiAlertCircle color="#c53030" size={20} />
                 </Error>
             )}
